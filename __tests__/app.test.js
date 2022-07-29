0814748d45a33a7b8a9105e7cfb42e9989743ad8
Aphenphos <MaxwillWinters@gmail.com>
2022-07-29 07:49:01 -0400

    some setup stuff

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,23 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/stuff should list some stuff', async () => {
+    const resp = await request(app).get('/cats');
+    expect(resp.body.length).toEqual(8);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    })
+    expect(res.body).toEqual(expected);
+  });
+  it('get /stuff/2 shows thing details', async () => {
+    const resp = await request(app).get('/cats/2');
+    console.log(resp.body);
+    expect(resp.body).toEqual({
+      id: '2',
+      name: 'thing 2',
+      dontGet: 'dont get this'
+    });
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,14 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+drop table if exists stuff;
+
+create table stuff (
+    id bigint not null,
+    name varchar not null,
+    dontGet varchar not null
+);
+
+insert into stuff (id, name, dontGet) values
+('1', 'mouse', 'do not get'),
+('2', 'keyboard', 'dont do it')
